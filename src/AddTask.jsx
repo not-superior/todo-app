d1d645132c4c511177bb4fdc8351953e2688dffc
superior <joystonlasrado27@gmail.com>
2022-12-03 22:34:23 +0530

    fixed modal for mobile devices

--- src/AddTask.jsx
+++ src/AddTask.jsx
@@ -1,19 +1,46 @@
 import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 import 'bootstrap/dist/css/bootstrap.min.css'
 
+
+const style = {
+    position: "relative"
+};
+
+const addbtnStyle = {
+    position: "absolute",
+    right: "90px",
+    bottom: "12px"
+};
+
+const inputStyle = {
+    position: "absolute",
+    left: '68px',
+    bottom: "10px",
+    textDecoration: 'none',
+    outline: 'none',
+    border: 'none',
+    padding: '5px',
+    backgroundColor: '#F2F2F2',
+    borderRadius: '6px'
+};
+
+const styleBody = {
+    display: 'flex'
+};
+
 const AddTask = ({openPopUp, statusModal, submitTask, setTask}) =>{
     return(
     <>
-    <Modal isOpen={openPopUp} toggle={()=>statusModal(false)}>
+    <Modal isOpen={openPopUp} style={style} toggle={()=>statusModal(false)}>
         <ModalHeader toggle={()=>statusModal(false)}>
             What's in your mind?
         </ModalHeader>
-        <ModalBody>
-            <label>Please Enter: </label>&nbsp;
-            <input onChange={(e)=>setTask({type: "SET_TASK", payload: e.target.value})}></input>&nbsp;
-            <Button color="primary" onClick={()=>{if(submitTask()){statusModal(false)}}}>Add Task</Button>
+        <ModalBody style={styleBody}>
+            <label>Enter: </label>&nbsp;
+            <input placeholder='Click to enter new task' style={inputStyle} onChange={(e)=>setTask({type: "SET_TASK", payload: e.target.value})}></input>&nbsp;
         </ModalBody>
         <ModalFooter>
+            <Button color="primary" style={addbtnStyle} onClick={()=>{if(submitTask()){statusModal(false)}}}>Add Task</Button>
             <Button color="danger" onClick={()=>statusModal(false)}>Close</Button>
         </ModalFooter>
     </Modal>
